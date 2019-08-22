7e01ba86944fb6b6b1ea84f99d6ad3443f1b3290
Giuseppe Lanza <gringoire986@gmail.com>
2019-08-22 10:50:32 +0100

    add capture operators to shared sequence

--- RxEnumKit/SharedSequence+CaseAccessible.swift
+++ RxEnumKit/SharedSequence+CaseAccessible.swift
@@ -19,5 +19,21 @@ public extension SharedSequence where Element: CaseAccessible {
         return filter { $0 ~= pattern }
     }
 
+    func exclude(case: Element) -> SharedSequence<SharingStrategy, Element> {
+        return filter { $0 !~= `case` }
+    }
+
+    func exclude<AssociatedValue>(case pattern: @escaping (AssociatedValue) -> Element) -> SharedSequence<SharingStrategy, Element> {
+        return filter { $0 !~= pattern }
+    }
     
+    func capture(case: Element) -> SharedSequence<SharingStrategy, Void> {
+        return filter { $0 ~= `case` }.map { _ in }
+    }
+    
+    func capture<AssociatedValue>(case pattern: @escaping (AssociatedValue) -> Element) -> SharedSequence<SharingStrategy, AssociatedValue> {
+        return asObservable()
+            .compactMap { $0[case: pattern] }
+            .asSharedSequence(onErrorRecover: { _ in .empty() })
+    }
 }
