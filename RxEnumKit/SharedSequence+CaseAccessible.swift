f0a43ec42a8a3c3e275e566e62a6ea1ddf831082
Giuseppe Lanza <gringoire986@gmail.com>
2019-08-22 11:23:20 +0100

    Add documentation for shared sequence extension

--- RxEnumKit/SharedSequence+CaseAccessible.swift
+++ RxEnumKit/SharedSequence+CaseAccessible.swift
@@ -11,41 +11,75 @@ import RxSwift
 import EnumKit
 
 public extension SharedSequence where Element: CaseAccessible {
+    /// Filters the elements of an observable sequence based on a specific case.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence that contains enum cases from the input sequence that satisfy the case condition.
     func filter(case: Element) -> SharedSequence<SharingStrategy, Element> {
         return filter { $0 ~= `case` }
     }
     
+    /// Filters the elements of an observable sequence based on a specific case pattern.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence that contains enum cases from the input sequence that satisfy the case condition.
     func filter<AssociatedValue>(case pattern: @escaping (AssociatedValue) -> Element) -> SharedSequence<SharingStrategy, Element> {
         return filter { $0 ~= pattern }
     }
 
+    /// Filters the elements of an observable sequence based on a specific case, excluding it from the stream.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence that contains enum cases from the input sequence that do not satisfy the case condition.
     func exclude(case: Element) -> SharedSequence<SharingStrategy, Element> {
         return filter { $0 !~= `case` }
     }
 
+    /// Filters the elements of an observable sequence based on a specific case pattern, excluding it from the stream.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence that contains enum cases from the input sequence that do not satisfy the case condition.
     func exclude<AssociatedValue>(case pattern: @escaping (AssociatedValue) -> Element) -> SharedSequence<SharingStrategy, Element> {
         return filter { $0 !~= pattern }
     }
     
+    /// Projects each matching enum case of an observable sequence into its associated value.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence whose elements are the result of associated value extraction on each matching cases of source.
     func capture(case: Element) -> SharedSequence<SharingStrategy, Void> {
         return filter { $0 ~= `case` }.map { _ in }
     }
     
+    /// Projects each matching enum case of an observable sequence into its associated value.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - returns: An observable sequence whose elements are the result of associated value extraction on each matching cases of source.
     func capture<AssociatedValue>(case pattern: @escaping (AssociatedValue) -> Element) -> SharedSequence<SharingStrategy, AssociatedValue> {
         return asObservable()
             .compactMap { $0[case: pattern] }
             .asSharedSequence(onErrorRecover: { _ in .empty() })
     }
     
+    /// Projects each matching enum case's associated value of an observable sequence into a new form.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - parameter transform: A transform function to apply to each source matching enum case's associated value.
+    /// - returns: An observable sequence whose elements are the result of invoking the transform function on each matching enum
+    /// case's associated value of the source.
     func map<T>(case: Element, _ transform: @escaping () -> T) -> SharedSequence<SharingStrategy, T> {
         return capture(case: `case`).map(transform)
     }
     
+    /// Projects each matching enum case's associated value of an observable sequence into a new form.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - parameter transform: A transform function to apply to each source matching enum case's associated value.
+    /// - returns: An observable sequence whose elements are the result of invoking the transform function on each matching enum
+    /// case's associated value of the source.
     func map<AssociatedValue, T>(case pattern: @escaping (AssociatedValue) -> Element,
                                  _ transfrom: @escaping (AssociatedValue) -> T) -> SharedSequence<SharingStrategy, T> {
         return capture(case: pattern).map(transfrom)
     }
 
+    /// Projects each matching enum case's associated value of an observable sequence into an optional form and filters all optional results.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - parameter transform: A transform function to apply to each source matching enum case's associated value and which returns an
+    /// element or nil.
+    /// - returns: An observable sequence whose elements are the result of filtering the transform function for each matching enum case's
+    /// associated value of the source.
     func compactMap<T>(case: Element, _ transform: @escaping () -> T?) -> SharedSequence<SharingStrategy, T> {
         return capture(case: `case`)
             .asObservable()
@@ -53,6 +87,12 @@ public extension SharedSequence where Element: CaseAccessible {
             .asSharedSequence(onErrorRecover: { _ in .empty() })
     }
     
+    /// Projects each matching enum case's associated value of an observable sequence into an optional form and filters all optional results.
+    /// - parameter case: An enum case to test each source element for a matching condition.
+    /// - parameter transform: A transform function to apply to each source matching enum case's associated value and which returns an
+    /// element or nil.
+    /// - returns: An observable sequence whose elements are the result of filtering the transform function for each matching enum case's
+    /// associated value of the source.
     func compactMap<AssociatedValue, T>(case pattern: @escaping (AssociatedValue) -> Element,
                                         _ transform: @escaping (AssociatedValue) -> T?) -> SharedSequence<SharingStrategy, T> {
         return capture(case: pattern)
@@ -72,7 +112,6 @@ public extension SharedSequence where Element == CaseAccessible {
     
 
     /// Filters the elements of an observable sequence based on a specific case.
-    /// - seealso: [filter operator on reactivex.io](http://reactivex.io/documentation/operators/filter.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - returns: An observable sequence that contains enum cases from the input sequence that satisfy the case condition.
     func filter<T: CaseAccessible>(case: T) -> SharedSequence<SharingStrategy, T> {
@@ -80,7 +119,6 @@ public extension SharedSequence where Element == CaseAccessible {
     }
     
     /// Filters the elements of an observable sequence based on a specific case pattern.
-    /// - seealso: [filter operator on reactivex.io](http://reactivex.io/documentation/operators/filter.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - returns: An observable sequence that contains enum cases from the input sequence that satisfy the case condition.
     func filter<T: CaseAccessible, AssociatedValue>(case pattern: @escaping (AssociatedValue) -> T) -> SharedSequence<SharingStrategy, T> {
@@ -88,7 +126,6 @@ public extension SharedSequence where Element == CaseAccessible {
     }
     
     /// Filters the elements of an observable sequence based on a specific case, excluding it from the stream.
-    /// - seealso: [filter operator on reactivex.io](http://reactivex.io/documentation/operators/filter.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - returns: An observable sequence that contains enum cases from the input sequence that do not satisfy the case condition.
     func exclude<T: CaseAccessible>(case: T) -> SharedSequence<SharingStrategy, T> {
@@ -96,7 +133,6 @@ public extension SharedSequence where Element == CaseAccessible {
     }
     
     /// Filters the elements of an observable sequence based on a specific case pattern, excluding it from the stream.
-    /// - seealso: [filter operator on reactivex.io](http://reactivex.io/documentation/operators/filter.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - returns: An observable sequence that contains enum cases from the input sequence that do not satisfy the case condition.
     func exclude<T: CaseAccessible, AssociatedValue>(case pattern: @escaping (AssociatedValue) -> T) -> SharedSequence<SharingStrategy, T> {
@@ -119,7 +155,6 @@ public extension SharedSequence where Element == CaseAccessible {
     }
     
     /// Projects each matching enum case's associated value of an observable sequence into a new form.
-    /// - seealso: [map operator on reactivex.io](http://reactivex.io/documentation/operators/map.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - parameter transform: A transform function to apply to each source matching enum case's associated value.
     /// - returns: An observable sequence whose elements are the result of invoking the transform function on each matching enum
@@ -129,7 +164,6 @@ public extension SharedSequence where Element == CaseAccessible {
     }
     
     /// Projects each matching enum case's associated value of an observable sequence into a new form.
-    /// - seealso: [map operator on reactivex.io](http://reactivex.io/documentation/operators/map.html)
     /// - parameter case: An enum case to test each source element for a matching condition.
     /// - parameter transform: A transform function to apply to each source matching enum case's associated value.
     /// - returns: An observable sequence whose elements are the result of invoking the transform function on each matching enum
